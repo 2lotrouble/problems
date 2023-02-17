78e8acf8f8076e50b88b103378c78f7b9c436515
2lotroubl <alldavl.work@gmail.com>
2023-02-18 00:11:38 +0300

    solved problems

---
+++ leetcode/206. Reverse Linked List/solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseList(self, head):
+        prev_node = None
+        current_node = head
+        while current_node:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+            print(current_node)
+        return prev_node
---
+++ leetcode/21. Merge Two Sorted Lists/solution.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        ans = dum = ListNode()
+        while list1 and list2:
+            if list1.val <= list2.val:
+                ans.next = list1
+                list1 = list1.next
+                ans = ans.next
+            else:
+                ans.next = list2
+                list2 = list2.next
+                ans = ans.next
+
+        if list1 or list2:
+            ans.next = list1 if list1 else list2
+
+        return dum.next
